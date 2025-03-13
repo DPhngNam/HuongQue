7e5ecfd9a382b6e34ff5ace9b9e2835927d6ca08
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2025-03-13 10:48:54 +0700

    connect Spring and postgre db

--- be_huongque/authservice/src/main/java/com/huongque/authservice/entity/User.java
+++ be_huongque/authservice/src/main/java/com/huongque/authservice/entity/User.java
@@ -1,23 +1,23 @@
 package com.huongque.authservice.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.security.Timestamp;
 import java.util.UUID;
 
-
 @Entity
-
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name='UUID',strategy = "org.hibernate.id.UUIDGenerator")
-    private UUID Ii;
+    @GenericGenerator(name= "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
 
     @Column(nullable = false,unique = true)
     private  String username;
@@ -27,11 +27,15 @@ public class User {
     @Column(nullable = false,unique = true)
     private  String email;
 
+    @Column(name = "oauth2_provider")
+    private String oauth2Provider;
+
+    @Column(name = "oauth2_id")
+    private String oauth2Id;
+
     @CreationTimestamp
     private Timestamp createdAt;
 
     @CreationTimestamp
     private Timestamp updatedAt;
-
-
 }
