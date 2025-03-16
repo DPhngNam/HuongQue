8be276b508da7ae4cb764088c26857453095b36a
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2025-03-16 16:37:00 +0700

    security filter

--- be_huongque/authservice/src/main/java/com/huongque/authservice/config/SecurityConfig.java
+++ be_huongque/authservice/src/main/java/com/huongque/authservice/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package com.huongque.authservice.config;
 
+import com.huongque.authservice.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserService userService;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserService userService) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    
 }
