b0cc3b5774e4ed49d077ef4cdab24bdc9d24479f
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-03-22 21:40:21 +0700

    feat: entity

---
+++ be_huongque/user-service/src/main/java/com/huongque/userservice/entity/UserAddress.java
@@ -0,0 +1,26 @@
+package com.huongque.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user_address")
+public class UserAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserProfile userProfile;
+
+    @Column(nullable = false)
+    private String address;
+}
