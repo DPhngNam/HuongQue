224ccdb849d37d934d36107822007566a7fe47fe
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-04-12 15:38:51 +0700

    Update main.ts

--- be_huongque/registerservice/src/main.ts
+++ be_huongque/registerservice/src/main.ts
@@ -2,17 +2,35 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Swagger setup (only available on HTTP server)
   const config = new DocumentBuilder()
     .setTitle('Register Service')
     .setDescription('The Register Service API description')
     .setVersion('1.0')
     .addTag('register')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Connect microservice
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'cats_queue',
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+
+  // Start both HTTP server and microservice
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
