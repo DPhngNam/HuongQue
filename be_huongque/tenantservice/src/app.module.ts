6ea2013a743005e1dfa48b956f8360a5df177da6
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-13 18:12:48 +0700

    feat: db config

--- be_huongque/tenantservice/src/app.module.ts
+++ be_huongque/tenantservice/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TenantModule } from './tenant/tenant.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TenantModule],
+  imports: [TenantModule,
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'tenant_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
