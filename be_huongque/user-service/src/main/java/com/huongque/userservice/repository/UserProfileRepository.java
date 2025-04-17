634bd1519992cc34cdea6242dfaf7ec1f1a1fdda
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-17 10:18:10 +0700

    fix: delete user controller

--- be_huongque/user-service/src/main/java/com/huongque/userservice/repository/UserProfileRepository.java
+++ be_huongque/user-service/src/main/java/com/huongque/userservice/repository/UserProfileRepository.java
@@ -3,7 +3,7 @@ package com.huongque.userservice.repository;
 import com.huongque.userservice.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+
 import java.util.UUID;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
