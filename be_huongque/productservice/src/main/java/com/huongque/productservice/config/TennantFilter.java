f1256025d4a743c904b27091a7202a0047e97327
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-04-21 19:43:25 +0700

    feat: middle ware filter tenant

--- be_huongque/productservice/src/main/java/com/huongque/productservice/config/TennantFilter.java
+++ be_huongque/productservice/src/main/java/com/huongque/productservice/config/TennantFilter.java
@@ -7,13 +7,24 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class TennantFilter extends OncePerRequestFilter {
 
     @Override
-    protected  void doFilterInternal(HttpServletRequest request,
-                                     HttpServletResponse response,
-        FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String tenantIdHeader = request.getHeader("X-Tenant-ID");
+        if(tenantIdHeader != null){
+            TenantContext.setTenantId(UUID.fromString(tenantIdHeader));
+
+        }
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            TenantContext.clear();
+        }
 
 
     }
