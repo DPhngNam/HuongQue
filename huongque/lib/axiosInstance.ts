2fb1d1946ec9cccb95e12c623c4fe6ce081db341
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-05-12 14:45:56 +0700

    feat: axios instance

---
+++ huongque/lib/axiosInstance.ts
@@ -0,0 +1,84 @@
+import { useAuthStore } from "@/app/stores/authStore";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.BASE_URL,
+});
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const { accessToken } = useAuthStore.getState();
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    const { refreshToken, setTokens, clearTokens } = useAuthStore.getState();
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (!refreshToken) {
+        clearTokens();
+        return Promise.reject(error);
+      }
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = "Bearer " + token;
+            return axiosInstance(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+      originalRequest._retry = true;
+      isRefreshing = true;
+      try {
+        const response = await axios.post(
+          `${process.env.BASE_URL}/auth/refresh-token`,
+          {
+            refreshToken,
+          }
+        );
+        const newAccessToken = response.data.accessToken;
+        const newRefreshToken = response.data.refreshToken;
+        setTokens(newAccessToken, newRefreshToken);
+        axiosInstance.defaults.headers.common["Authorization"] =
+          "Bearer " + newAccessToken;
+        processQueue(null, newAccessToken);
+
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        clearTokens();
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
