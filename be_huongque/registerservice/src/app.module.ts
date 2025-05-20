d14d1a52f5feaf360f2585943081a2d695c0bbb5
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-05-21 02:12:14 +0700

    config rabbitMQ for register

--- be_huongque/registerservice/src/app.module.ts
+++ be_huongque/registerservice/src/app.module.ts
@@ -3,16 +3,30 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegistrationsModule } from './module/registrations/registrations.module';
-
+import { RabbitmqModule } from './module/rabbitmq/rabbitmq.module';
+import { RabbitmqService } from './module/rabbitmq/rabbitmq.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '../.env', // Specify the path to your .env file
-      isGlobal: true, // Makes the ConfigModule global (optional)
+      envFilePath: '../.env',
+      isGlobal: true,
     }),
-    RegistrationsModule
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    RegistrationsModule,
+    RabbitmqModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RabbitmqService],
 })
-export class AppModule {}
+export class AppModule { }
