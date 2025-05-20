abd5d44f146d0c447f9a9401e7e8835ca0aaa67d
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2025-05-21 02:11:55 +0700

    reinit orderservice

---
+++ be_huongque/orderservice/src/main/java/com/huongque/orderservice/dto/createOrderDto.java
@@ -0,0 +1,30 @@
+package com.huongque.orderservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Data
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class createOrderDto {
+    private String orderId;
+    private String customerName;
+    private String product;
+    private int quantity;
+    private double price;
+    private String orderDate;
+
+    
+}
