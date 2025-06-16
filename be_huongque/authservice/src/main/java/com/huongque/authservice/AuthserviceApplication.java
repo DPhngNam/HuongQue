15311d5b1797e7efad86e122e9947b4d10853ba5
DPhngNam <22520908@gm.uit.edu.vn>
2025-06-17 02:12:07 +0700

    fix: :bug: connection to eureka

--- be_huongque/authservice/src/main/java/com/huongque/authservice/AuthserviceApplication.java
+++ be_huongque/authservice/src/main/java/com/huongque/authservice/AuthserviceApplication.java
@@ -1,10 +1,15 @@
 package com.huongque.authservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.huongque.authservice.repository")
@@ -12,6 +17,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 public class AuthserviceApplication {
 
+	@Autowired
+	private Environment env;
+	
+	@PostConstruct
+	public void printEurekaUrl() {
+		System.out.println(">>> Eureka URL = " + env.getProperty("spring.eureka.client.service-url.defaultZone"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserviceApplication.class, args);
 	}
