8f34c32e42ba5de19254497d49740f6c79f8441b
DPhngNam <22520908@gm.uit.edu.vn>
2025-06-17 23:28:39 +0700

    refractor code

--- be_huongque/productservice/src/main/java/com/huongque/productservice/data/CategorySeeder.java
+++ be_huongque/productservice/src/main/java/com/huongque/productservice/data/CategorySeeder.java
@@ -18,20 +18,23 @@ public class CategorySeeder implements CommandLineRunner {
     private CategoryRepository categoryRepository;
 
     @Override
-public void run(String... args) throws Exception {
-    if (categoryRepository.count() == 0) {
-        List<Category> categories = List.of(
-            new Category(UUID.fromString("24d67553-552f-4225-9c62-b281b7a5bb19"), "Thực phẩm - Ẩm thực", "thuc-pham-am-thuc"),
-            new Category(UUID.fromString("af62bf01-fafa-4225-9db9-ab40e724f59c"), "Đồ Uống", "do-uong"),
-            new Category(UUID.fromString("364cf7f8-7aea-4539-bf72-bbec2aa84ea4"), "Thảo Dược", "thao-duoc"),
-            new Category(UUID.fromString("2f6e9ce4-2086-48aa-a7ad-e67b087744c8"), "Vải và may mặc", "vai-va-may-mac"),
-            new Category(UUID.fromString("6316c8a9-38dd-4696-a19d-44e5b98aa368"), "Nội thất - Trang trí - Lưu niệm", "noi-that-trang-tri-luu-niem"),
-            new Category(UUID.fromString("d67b103a-22eb-4bd1-8dcb-95acd385bf49"), "Dịch vụ", "dich-vu"),
-            new Category(UUID.fromString("5049fd36-c3a3-4a6b-b4db-c49a01935f49"), "Các sản phẩm khác", "cac-san-pham-khac")
-        );
+    public void run(String... args) throws Exception {
+        if (categoryRepository.count() == 0) {
+            List<Category> categories = List.of(
+                    new Category(UUID.fromString("24d67553-552f-4225-9c62-b281b7a5bb19"), "Thực phẩm - Ẩm thực",
+                            "thuc-pham-am-thuc"),
+                    new Category(UUID.fromString("af62bf01-fafa-4225-9db9-ab40e724f59c"), "Đồ Uống", "do-uong"),
+                    new Category(UUID.fromString("364cf7f8-7aea-4539-bf72-bbec2aa84ea4"), "Thảo Dược", "thao-duoc"),
+                    new Category(UUID.fromString("2f6e9ce4-2086-48aa-a7ad-e67b087744c8"), "Vải và may mặc",
+                            "vai-va-may-mac"),
+                    new Category(UUID.fromString("6316c8a9-38dd-4696-a19d-44e5b98aa368"),
+                            "Nội thất - Trang trí - Lưu niệm", "noi-that-trang-tri-luu-niem"),
+                    new Category(UUID.fromString("d67b103a-22eb-4bd1-8dcb-95acd385bf49"), "Dịch vụ", "dich-vu"),
+                    new Category(UUID.fromString("5049fd36-c3a3-4a6b-b4db-c49a01935f49"), "Các sản phẩm khác",
+                            "cac-san-pham-khac"));
 
-        categoryRepository.saveAll(categories);
-        System.out.println("✅ Category seeding completed successfully.");
+            categoryRepository.saveAll(categories);
+            System.out.println("✅ Category seeding completed successfully.");
+        }
     }
-}
 }
--- be_huongque/productservice/src/main/java/com/huongque/productservice/entity/Category.java
+++ be_huongque/productservice/src/main/java/com/huongque/productservice/entity/Category.java
@@ -1,50 +1,52 @@
 package com.huongque.productservice.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="category")
+@Table(name = "category")
 @Data
 public class Category {
     @Id
     private UUID id;
 
-    @Column(name="name",nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-     @Column(unique = true)
+    @Column(unique = true)
     private String slug;
 
-
-
-   @CreationTimestamp // Automatically sets the timestamp on entity creation
-    @Column(name = "created_at", nullable = false, updatable = false) // Ensures it's not null and can't be changed after creation
+    @CreationTimestamp // Automatically sets the timestamp on entity creation
+    @Column(name = "created_at", nullable = false, updatable = false) // Ensures it's not null and can't be changed
+                                                                      // after creation
     private LocalDateTime createdAt;
 
     @UpdateTimestamp // Automatically updates the timestamp on entity modification
     @Column(name = "updated_at", nullable = false) // Ensures it's not null and updates
     private LocalDateTime updatedAt;
-public Category(UUID id, String name) {
-    this.id = id;
-    this.name = name;
-}
-public Category(UUID id, String name, String slug) {
-    this.id = id;
-    this.name = name;
-    this.slug = slug;
-}
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(UUID id, String name, String slug) {
+        this.id = id;
+        this.name = name;
+        this.slug = slug;
+    }
 
 }
