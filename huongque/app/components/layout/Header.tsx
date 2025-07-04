a15432d972b29f6f431211822d5b66ec98f671f9
DPhngNam <22520908@gm.uit.edu.vn>
2025-07-04 10:30:34 +0000

    style: :lipstick: header has w-full

--- huongque/app/components/layout/Header.tsx
+++ huongque/app/components/layout/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="w-full flex h-16 items-center px-4">
         <div className="mr-4 flex">
           <Image
             src="/image/logo.png"
@@ -46,7 +46,7 @@ export default function Header() {
             <NavMenu />
           </nav>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+        <div className="flex flex-1 items-center justify-between md:justify-end">
           <div className="w-full flex-1 md:w-auto md:flex-none">
             <div className="relative">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
